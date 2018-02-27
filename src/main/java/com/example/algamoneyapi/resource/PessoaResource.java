0265d8d71dcde4e23cdb6aa9b0b3be9d1abd52c8
devmarcoslira <marcoslira.ti@gmail.com>
2018-02-27 17:02:19 -0300

    Implementando atualização parcial com PUT

--- src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/PessoaResource.java
@@ -78,5 +78,11 @@ public class PessoaResource {
 		return ResponseEntity.ok(pessoaSalva);//retorna o status ok no http request
 	}
 	
+	@PutMapping("/{codigo}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
+	
 
 }
--- src/main/java/com/example/algamoneyapi/service/PessoaService.java
+++ src/main/java/com/example/algamoneyapi/service/PessoaService.java
@@ -15,12 +15,25 @@ public class PessoaService {
 	private PessoaRepository pessoaRepository;
 	
 	public Pessoa Atualizar(Long codigo, Pessoa pessoa) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo"); //copia as propriedades da entidade pessoa de acordo com o código
+		return pessoaRepository.save(pessoaSalva);
+	}
+
+	
+
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+	}
+	
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
 		Pessoa pessoaSalva = pessoaRepository.findOne(codigo);
 		if (pessoaSalva == null) {
 			throw new EmptyResultDataAccessException(1);
 		}
-		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo"); //copia as propriedades da entidade pessoa de acordo com o código
-		return pessoaRepository.save(pessoaSalva);
+		return pessoaSalva;
 	}
 
 }
